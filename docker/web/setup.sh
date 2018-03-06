d99a8a9d610371c00d25c48973eaf0764c7552fd
ninjafx <bilal.islam815@gmail.com>
2018-03-06 22:21:40 +0300

    json has been fixe

--- docker/web/setup.sh
+++ docker/web/setup.sh
@@ -9,4 +9,4 @@ done
 
 curl  -XPUT 'http://elk:9200/product?pretty' -H 'Content-Type: application/json' -d' {"settings" : {"number_of_shards" : 1},"mappings" : {"product" : {"properties" : {"id" : { "type" : "keyword" },"name" : { "type" : "text" },"lastUpdateDate" : { "type" : "date" }}}},"aliases" : {"alias-product" : {}}}' -o /dev/null
 #curl -XPOST 'http://elk:9200/product/doc/_bulk?pretty' -H 'Content-Type: application/json' --data-binary @product.json
-dotnet Equinox.WebApi.dll
+dotnet Equinox.UI.Site.dll
